4bb63dd879e9fd7ed58bf2fd903c2d5807190036
L&J <609069481@qq.com>
2022-10-13 03:03:04 +0800

    first

---
+++ src/Test.java
@@ -0,0 +1,26 @@
+/**
+ *  测试类
+ * <p>
+ *  中文测试, 你好, 中国!
+ * @author L_J
+ * @since 2022/10/7 21:19
+ */
+public class Test {
+    public static final String sfs = "abc";
+    private String field;
+
+    public Test(String field) {
+        System.out.println(field);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world" + (1 != 2));
+        Integer localVar = 1;
+        System.out.println("localVar = " + localVar);
+        if (6 >= 5) {
+        }
+        System.out.println("abc = " + sfs);
+        System.out.println(false);
+        // 0OoIJijk961l1L
+    }
+}
